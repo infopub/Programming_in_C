27e216b61e014782301a1c0868b620c12732a1f1
infopub <webmaster@infopub.co.kr>
2015-06-17 17:30:25 +0900

    initial commit

---
+++ ch19/program1901.c
@@ -0,0 +1,19 @@
+// Simple program to work with fractions
+#include <stdio.h>
+
+typedef struct {
+    int numerator;
+    int denominator;
+} Fraction;
+
+int main (void)
+{
+     Fraction myFract;
+
+     myFract.numerator = 1;
+     myFract.denominator = 3;
+
+     printf ("The fraction is %i/%i\n", myFract.numerator, myFract.denominator);
+
+     return 0;
+}
